09c94e399440a6b4755e86a2bf6497dc42acb806
Dr_Dee <digitalis13gmx.de>
2021-03-04 17:06:11 +0100

    v1.1.0

--- scripts/popup/main.js
+++ scripts/popup/main.js
@@ -2,6 +2,10 @@ let currentState = 'add'
 
 let accounts
 
+const params = new URLSearchParams(window.location.search)
+if (params.has('msg')) {
+    showMessage(params.get('msg'))
+}
 populateAccounts()
 $('input[type=text]').change(() => {
     $('#invalid-id').hide()
@@ -10,8 +14,10 @@ $('input[type=text]').change(() => {
 
 $('#options').change(() => {
     let toChange
+    let user = false
     if ($($("select#options option:selected").get(0)).hasClass('user')) {
         toChange = 'user'
+        user = $($("select#options option:selected").get(0)).val()
     } else {
         toChange = $($("select#options option:selected").get(0)).attr('id')
     }
@@ -22,18 +28,39 @@ $('#options').change(() => {
             $('#id-input').show()
             $('#delete-input').hide()
             $('#input-type').show()
+            $('#class-select').hide()
+            $('#account-type').hide()
             break
         case 'delete':
             $('#name-input').hide()
             $('#id-input').hide()
             $('#delete-input').show()
             $('#input-type').hide()
+            $('#class-select').hide()
+            $('#account-type').hide()
             break
         case 'user':
-            $('#name-input').hide()
-            $('#id-input').show()
-            $('#delete-input').hide()
-            $('#input-type').hide()
+            getAccounts().then(accts => {
+                console.log(accts)
+                accts.forEach(acc => {
+                    console.log(acc)
+                    if (acc.name === user) {
+                        if (acc.type === 'teacher') {
+                            $('#account').text('Lehrer')
+                            $('#class-select').show()
+                            $('#class-select').attr('href', 'classes.html?id=' + acc.id + '&name=' + acc.name)
+                        } else {
+                            $('#account').text('Schüler')
+                        }
+                        $('#id').val(acc.id)
+                    }
+                })
+                $('#name-input').hide()
+                $('#id-input').show()
+                $('#delete-input').hide()
+                $('#input-type').hide()
+                $('#account-type').show()
+            })
 
     }
 })
@@ -75,10 +102,11 @@ $('#save').click(() => {
                             accounts.push({
                                 name: $('#name').val(),
                                 id: isValid,
-                                type: 'teacher'
+                                type: 'teacher',
+                                classes: []
                             })
                             saveAccounts(accounts)
-                            window.location.replace('classes.html?id=' + isValid + '&name=' + $('#name').val())
+                            window.location.replace('classes.html?newCreated=&id=' + isValid + '&name=' + $('#name').val())
                         } else {
                             $('#invalid-id').show()
                         }
@@ -89,12 +117,14 @@ $('#save').click(() => {
         }
         case 'delete': {
             const id = $('#delete-select').val()
-            accounts = accounts.filter((element) => {
-                return element.name !== id
+            askForConfirm('Möchten Sie den Account ' + id + ' wirklich löschen?', () => {
+                accounts = accounts.filter((element) => {
+                    return element.name !== id
+                })
+                saveAccounts(accounts)
+                populateAccounts()
+                showMessage('Der Account ' + id + ' wurde erfolgreich gelöscht!')
             })
-            saveAccounts(accounts)
-            populateAccounts()
-            showMessage('Der Account ' + id + ' wurde erfolgreich gelöscht!')
             break
         }
 
@@ -129,25 +159,51 @@ function populateAccounts() {
                 $('#options option[selected]').removeAttr('selected')
                 $('#add').attr('selected', 'true')
             }
-            
+
             accounts = acc
         } else {
             accounts = []
         }
     })
 
-   
+
 }
 
 $('#close-msg').click(() => {
     $('#message').hide()
-    $('#content').fadeIn(1000) 
+    $('#content').fadeIn(1000)
     $('body').height('100%')
 })
 
-function showMessage(msg){
+function showMessage(msg, buttonMsg = 'Okay') {
+    $('#overlay-content').text(msg)
+    $('#close-msg').text(buttonMsg)
+    $('body').height($('#content').height())
+    $('#message').fadeIn(1000)
+    $('#content').hide()
+    $('#close-msg').show()
+    $('#confirm').hide()
+}
+
+function askForConfirm(msg, yes = () => {}, no = () => {}) {
     $('#overlay-content').text(msg)
     $('body').height($('#content').height())
     $('#message').fadeIn(1000)
     $('#content').hide()
+    $('#close-msg').hide()
+    $('#confirm').show()
+    $('#yes').click(() => {
+        $('#yes').off('click')
+        $('#message').hide()
+        $('#content').fadeIn(1000)
+        $('body').height('100%')
+        yes()
+    })
+    $('#no').click(() => {
+        $('#no').off('click')
+        $('#message').hide()
+        $('#content').fadeIn(1000)
+        $('body').height('100%')
+        no()
+    })
 }
