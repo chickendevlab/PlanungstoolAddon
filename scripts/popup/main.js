dd8fe9ab6df4a92ba073cbb53820e80acdf2fb7c
Dr_Dee <digitalis13gmx.de>
2021-03-06 15:04:46 +0100

    v2.0.0

--- scripts/popup/main.js
+++ scripts/popup/main.js
@@ -2,16 +2,29 @@ let currentState = 'add'
 
 let accounts
 
+// Um Benachrichtigungen anzeigen zu lassen, einfach settings.html mit ?msg=<Nachricht hier> öffnen
 const params = new URLSearchParams(window.location.search)
 if (params.has('msg')) {
     showMessage(params.get('msg'))
 }
+// Lade alle Accounts
 populateAccounts()
+
+// Verstecke Fehlermeldung bei Inhaltsänderung
 $('input[type=text]').change(() => {
     $('#invalid-id').hide()
     $('#invalid-name').hide()
 })
 
+// Listener, welcher den Input für Namen versteckt/zeigt, je nachdem, ob man "Lehrer" oder "Schüler" auswählt
+$('#type').change(() => {
+    if ($('#type').val() === 'teacher') {
+        $('#name-input').show()
+    } else {
+        $('#name-input').hide()
+    }
+})
+// Ändere Menü-Items beim Ändern der Optionen
 $('#options').change(() => {
     let toChange
     let user = false
@@ -22,9 +35,17 @@ $('#options').change(() => {
         toChange = $($("select#options option:selected").get(0)).attr('id')
     }
     currentState = toChange
+    // Zeige benötigte und verstecke unnötige Elemente
     switch (toChange) {
         case 'add':
             $('#name-input').show()
+            
+            // Zeige Email-Input, wenn "Lehrer" ausgewählt ist
+            if ($('#type').val() === 'teacher') {
+                $('#name-input').show()
+            } else {
+                $('#name-input').hide()
+            }
             $('#id-input').show()
             $('#delete-input').hide()
             $('#input-type').show()
@@ -43,7 +64,7 @@ $('#options').change(() => {
             getAccounts().then(accts => {
                 console.log(accts)
                 accts.forEach(acc => {
-                    console.log(acc)
+                    // Zeige an, ob der Account ein Schüler oder ein Lehreraccount ist
                     if (acc.name === user) {
                         if (acc.type === 'teacher') {
                             $('#account').text('Lehrer')
@@ -75,39 +96,53 @@ $('#save').click(() => {
         case 'add': {
             const name = $('#name').val()
             let b = true
+            // Überprüfe, dass der eingegebene Account-Name nicht bereits in Verwendung ist
+            // Dies hat keinen Einfluss auf Schüleraccounts, da diese nicht mit der LernSax-Email, sondern mit dem Klassennamen verknüpft sind
             accounts.forEach(element => {
                 if (element.name === name) {
                     $('#invalid-name').show()
                     b = false
                 }
             })
-
             if (b) {
+                // Lehrer oder Schüler?
                 const type = $('#type').val()
                 if (type === 'student') {
-                    validateId($('#id').val()).then((isValid) => {
-                        if (isValid) {
+                    // Überprüfe Klassen-ID und lade Informationen
+                    validateId($('#id').val()).then((data) => {
+                        if (data.valid) {
+                            // Entferne alle Accounts für die bestimmte Klasse
+                            accounts = accounts.filter(el => {
+                                return el.name !== data.name
+                            })
+                            // Füge die ID als Account hinzu
                             accounts.push({
-                                name: $('#name').val(),
-                                id: isValid,
+                                name: data.name,
+                                id: data.id,
                                 type: 'student'
                             })
+
                             saveAccounts(accounts)
                             populateAccounts()
-                            showMessage('Der neue Account wurde erfolgreich angelegt.')
+                            showMessage('Die neue ID wurde erfolgreich angelegt.')
                         } else {
                             $('#invalid-id').show()
                         }
                     })
                 } else {
+                    // Überprüfe Lehrerkennung
                     validateTeacherId($('#id').val()).then(isValid => {
                         if (isValid) {
+                            // Füge Lehrerkennung zu Accounts hinzu
                             accounts.push({
                                 name: $('#name').val(),
                                 id: isValid,
                                 type: 'teacher'
                             })
+
+                            // Lade Klassen-IDs in den Cache
                             loadClassesWithTeacherId(isValid)
+                            // Speichere Accounts
                             saveAccounts(accounts)
                             showMessage('Der neue Account wurde erfolgreich angelegt.')
                         } else {
@@ -121,73 +156,82 @@ $('#save').click(() => {
         case 'delete': {
             const id = $('#delete-select').val()
             askForConfirm('Möchten Sie den Account ' + id + ' wirklich löschen?', () => {
+                //Entferne Account
                 accounts = accounts.filter((element) => {
                     return element.name !== id
                 })
-                
+
+                //Speichere Accounts ohne dem gelöschten Account
                 saveAccounts(accounts)
-                accounts = accounts.filter((element) => {
-                    return element.type !== 'student'
+
+                // Kopiere Accounts (Objekt zu JSON; JSON zu neuem Objekt)
+                let copy = JSON.parse(JSON.stringify(accounts))
+
+                // Entferne alle Schüleraccounts
+                copy = copy.filter((element) => {
+                    return element.type === 'teacher'
                 })
-                if(accounts.length == 0){
+
+                // Wenn kein Lehreraccount mehr verfügbar
+                if (copy.length == 0) {
+                    // ...lösche Klassen
                     deleteClasses()
                 }
+
+                //Aktualisiere Accounts
                 populateAccounts()
                 showMessage('Der Account ' + id + ' wurde erfolgreich gelöscht!')
             })
             break
         }
 
+        // Behandelt das Aktualisieren der Accountinformationen
+        // Nur für Lehreraccounts möglich, wenn sich eine Klassen-ID ändert, muss man die Klasse einfach nochmal löschen und dann neu hinzufügen
         case 'user': {
-            switch ($('#account').text()) {
-                case 'Schüler':
-                    validateId($('#id').val()).then(isValid => {
-                        if (isValid) {
-                            const sel = $('#options').val()
-                            accounts.forEach(element => {
-                                if (element.name === sel) {
-                                    element.id = isValid
-                                }
-                            })
-                            saveAccounts(accounts)
-                            showMessage('Die ID des Accounts ' + sel + ' wurde erfolgreich zu "' + isValid + '" geändert.')
-                        } else {
-                            $('#invalid-id').show()
+            // Überprüfe neue ID
+            validateTeacherId($('#id').val()).then(isValid => {
+                if (isValid) {
+                    const sel = $('#options').val()
+                    // Update Accountinfos
+                    accounts.forEach(element => {
+                        if (element.name === sel) {
+                            element.id = isValid
                         }
                     })
-                    break
-                case 'Lehrer': {
-                    validateTeacherId($('#id').val()).then(isValid => {
-                        if (isValid) {
-                            const sel = $('#options').val()
-                            accounts.forEach(element => {
-                                if (element.name === sel) {
-                                    element.id = isValid
-                                }
-                            })
-                            loadClassesWithTeacherId(isValid)
-                            saveAccounts(accounts)
-                            showMessage('Die ID des Accounts ' + sel + ' wurde erfolgreich zu "' + isValid + '" geändert.')
-                        } else {
-                            $('#invalid-id').show()
-                        }
-                    })
+                    loadClassesWithTeacherId(isValid)
+                    saveAccounts(accounts)
+                    showMessage('Die ID des Accounts ' + sel + ' wurde erfolgreich zu "' + isValid + '" geändert.')
+                } else {
+                    $('#invalid-id').show()
                 }
-            }
+            })
+
+
 
         }
 
     }
 })
 
+
+// Funktion zum Anzeigen aller Accounts
 function populateAccounts() {
     getAccounts().then(acc => {
         if (acc) {
+
+            // Leere alle DropDown-Menüs
             $('#users').empty()
             $('#delete-select').empty()
+
+            // Iteriere über Accounts
             for (i = 0; i < acc.length; i++) {
-                $('#users').append('<option value="' + acc[i].name + '" class="user">' + acc[i].name + '</option>')
+                // Füge Lehreraccounts zu den Optionen hinzu
+                if (acc[i].type === 'teacher') { $('#users').append('<option value="' + acc[i].name + '" class="user">' + acc[i].name + '</option>') }
+
+                // Füge alle Accounts zum Delete-DropDown hinzu
                 $('#delete-select').append('<option value="' + acc[i].name + '" class="user">' + acc[i].name + '</option>')
+
+                // Wähle "Hinzufügen" aus
                 $('#options option[selected]').removeAttr('selected')
                 $('#add').attr('selected', 'true')
             }
@@ -201,12 +245,14 @@ function populateAccounts() {
 
 }
 
+// Listener für den Close-Button des Modals
 $('#close-msg').click(() => {
     $('#message').hide()
     $('#content').fadeIn(1000)
     $('body').height('100%')
 })
 
+// Funktion für ein costum-alert()
 function showMessage(msg, buttonMsg = 'Okay') {
     $('#overlay-content').text(msg)
     $('#close-msg').text(buttonMsg)
@@ -217,6 +263,7 @@ function showMessage(msg, buttonMsg = 'Okay') {
     $('#confirm').hide()
 }
 
+// Funktion für ein costum-confirm()
 function askForConfirm(msg, yes = () => { }, no = () => { }) {
     $('#overlay-content').text(msg)
     $('body').height($('#content').height())
